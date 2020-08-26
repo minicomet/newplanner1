27fa5add407b6ba30f6a2965e00c8cecb0d6fad5
minicomet <56183155+minicomet@users.noreply.github.com>
2020-08-26 19:04:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/newplanner/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.newplanner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.borax12.materialdaterangepicker.date.DatePickerDialog;
+import com.borax12.materialdaterangepicker.time.RadialPickerLayout;
+import com.borax12.materialdaterangepicker.time.TimePickerDialog;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
+import com.prolificinteractive.materialcalendarview.format.DateFormatDayFormatter;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    int hour, minute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MaterialCalendarView materialCalendarView = (MaterialCalendarView)findViewById(R.id.view);
+
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator(),
+                new oneDayDecorator());
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                AlertDialog.Builder plans = new AlertDialog.Builder(MainActivity.this);
+
+
+                plans.setTitle(date.getMonth()+"월 "+date.getDay()+"일");
+                LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+                View view = inflater.inflate(R.layout.plans, null);
+                plans.setView(view);
+
+                plans.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                plans.setNegativeButton("추가", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        AlertDialog.Builder sch = new AlertDialog.Builder(MainActivity.this);
+                        sch.setIcon(R.mipmap.ic_launcher);
+                        sch.setTitle("일정 추가");
+                        LayoutInflater inflater =  (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+                        View view = inflater.inflate(R.layout.add_schedule_dialog, null);
+                        sch.setView(view);
+
+
+                        sch.setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                AlertDialog.Builder addPlan = new AlertDialog.Builder(MainActivity.this);
+
+
+                            }
+                        });
+
+                        sch.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        });
+
+                        sch.show();
+                    }
+
+                });
+
+                plans.show();
+            }
+        });
+
+        RadioGroup plantype = (RadioGroup)findViewById(R.id.plantype);
+
+
+
+
+    }
+
+    public void editdate(View v){
+        AlertDialog.Builder date = new AlertDialog.Builder(MainActivity.this);
+        date.setIcon(R.mipmap.ic_launcher);
+        LayoutInflater inflater =  (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.calendar, null);
+        date.setView(view);
+        date.setNeutralButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+
+            }
+        });
+
+        date.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        date.show();
+    }
+
+    public void dateselect(View v){
+        Calendar now = Calendar.getInstance();
+        DatePickerDialog dpd = DatePickerDialog.newInstance(MainActivity.this, now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH));
+        dpd.show(getFragmentManager(), null);
+    }
+
+    public void timeselect(View v){
+        Calendar now = Calendar.getInstance();
+        TimePickerDialog tpd = TimePickerDialog.newInstance(MainActivity.this, now.get(Calendar.HOUR_OF_DAY), now.get(Calendar.MINUTE), false);
+        tpd.show(getFragmentManager(), null);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth, int yearEnd, int monthOfYearEnd, int dayOfMonthEnd) {
+
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int hourOfDayEnd, int minuteEnd) {
+
+    }
+}
